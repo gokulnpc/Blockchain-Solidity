94948ad86d8feddb4fc9eaf0b789714e12c61082
gokulnpc <gokulvbn123@gmail.com>
2022-09-19 10:55:58 +0530

    TodoList

--- Blockchain/SimpleStorage/deploy.py
+++ Blockchain/SimpleStorage/deploy.py
@@ -1,10 +1,9 @@
 from solcx import compile_standard, install_solc
 
 install_solc("0.6.0")
-
 import json
 from web3 import Web3
-#web3
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
@@ -37,4 +36,21 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # for connecting to ganache
 # HTTPProvider:
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 5777
+chain_id = 1337
+my_address = "0xEAb528e16e5Bb4a9C81E8729F78Ef9C3Ff4b9Ee1"
+private_key = "0xbf05d89550d010090be19cdc12ccea71f88220930c4f8083859c7a886d339d6b"
+
+# create contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# 1. build a transaction
+# 2. sign a transaction
+# 3. send a transaction
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+print(transaction)
---
+++ Blockchain/ToDoList/contracts/TodoList.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList
+{
+    uint public taskCount = 0;
+    
+
+}
