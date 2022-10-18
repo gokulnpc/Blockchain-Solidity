af5b3f596e6e81dd288a5d45a14505ed5668678b
gokulnpc <gokulvbn123@gmail.com>
2022-10-18 12:38:09 +0530

    nft app

---
+++ Blockchain/NFT-Music-player-app/src/backend/contracts/MusicNFTMarketplace.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+//importing contract from openzeppelin library
+//install: npm install @openzeppelin/contracts
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+//use nftstorage to upload music file to get link
+//paste those links in json file
+//then upload all those json files to car.ipfs to get car file
+//then upload that car file to nftstorage to get link
+
+contract MusicNFTMarketplace is ERC721("npcToken","NPC"), Ownable
+{
+    string public baseURI = "https://bafybeiboclp6bev4s4v6mtjp3os42ceivv6djcqeo3ymvxw3ln6kksaenq.ipfs.nftstorage.link/";
+    string public baseExtension = ".json";
+    
+    address public artist;
+    uint256 public royaltyFee;
+
+    //to sell nft at some price
+    struct MarketItems
+    {
+        uint256 tokenId;
+
+        //inorder for the owner address to receive ether as the payment
+        //it should be payable
+        address payable seller;
+        uint256 price;
+    } 
+
+    //array of structures
+    MarketItems[] public marketItems;
+
+    //this contract will be deployed by the artist
+    //_prices[] contains price of each of the nft
+    //_royalty fees is amount that should be paid to artist
+    //address _ artist is wallet address of the artist
+    constructor(uint256 _royaltyFee,address _artist,uint256[] memory _prices) payable
+    {
+        require(_prices.length * _royaltyFee <= msg.value);
+        royaltyFee=_royaltyFee;
+        artist = _artist;
+        for(uint8 i = 0 ; i < _prices.length ; i++)
+        {
+            require(_prices[i] > 0,"Prices must be greater than zero");
+            _mint(address(this),i); 
+            marketItems.push(MarketItems(i,payable(msg.sender),_prices[i]));
+        }
+    }
+
+    function buyNFT(uint256 _tokenId) public payable
+    {
+        require(_tokenId < marketItems.length,"not correct token id");
+        uint256 _amount = msg.value;
+        require(_amount == marketItems[_tokenId].price,"values should match");
+
+    }
+
+
+}
